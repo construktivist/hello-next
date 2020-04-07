b4a9db6cb5eab97f5060e6322e4f816fadebaea3
Keegan Kelly <keegankelly1@gmail.com>
2020-04-07 13:41:14 -0500

    Added Blog component data and component to index. Added component styles.

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,85 @@ import fetch from 'isomorphic-unfetch';
 //Components
 import Layout from '../components/MyLayout';
 
-// ======== FETCHING DATA FOR PAGES =========
+// ======== STYLING COMPONENTS =========
 
-const Index = props => (
-    <Layout>
-        <h1>Batman TV Shows</h1>
-        <ul>
-            {props.shows.map(show => (
-                <li key={show.id}>
-                    <Link href="/p/[id]" as={`/p/${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </Layout>
-)
-
-Index.getInitialProps = async function() {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-    const data = await res.json();
-
-    console.log(`Show data fetched. Count: ${data.length}`);
-
-    return {
-        shows: data.map(entry => entry.show)
-    };
+function getPosts() {
+    return [
+        {id: 'hello-next.js', title: 'Hello Next.js'},
+        {id: 'learn-next.js', title: 'Learn Next.js is awesome'},
+        {id: 'hello-next.js', title: 'Deploy apps with ZEIT'}
+    ]
 }
 
-export default Index;
+export default function Blog() {
+    return (
+        <Layout>
+        <h1>My Blog</h1>
+        <ul>
+          {getPosts().map(post => (
+            <li key={post.id}>
+              <Link href="/p/[id]" as={`/p/${post.id}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <style jsx>{`
+          h1,
+          a {
+            font-family: 'Arial';
+          }
+  
+          ul {
+            padding: 0;
+          }
+  
+          li {
+            list-style: none;
+            margin: 5px 0;
+          }
+  
+          a {
+            text-decoration: none;
+            color: blue;
+          }
+  
+          a:hover {
+            opacity: 0.6;
+          }
+        `}</style>
+      </Layout>
+    )
+}
+// ======== FETCHING DATA FOR PAGES =========
+
+// const Index = props => (
+//     <Layout>
+//         <h1>Batman TV Shows</h1>
+//         <ul>
+//             {props.shows.map(show => (
+//                 <li key={show.id}>
+//                     <Link href="/p/[id]" as={`/p/${show.id}`}>
+//                         <a>{show.name}</a>
+//                     </Link>
+//                 </li>
+//             ))}
+//         </ul>
+//     </Layout>
+// )
+
+// Index.getInitialProps = async function() {
+//     const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//     const data = await res.json();
+
+//     console.log(`Show data fetched. Count: ${data.length}`);
+
+//     return {
+//         shows: data.map(entry => entry.show)
+//     };
+// }
+
+// export default Index;
 
 // ======== LINKS WITH DYNAMIC ROUTING =========
 //

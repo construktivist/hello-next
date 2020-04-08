5dbebbf0499bdc60362e4b163772ae8fdbaefc24
Keegan Kelly <keegankelly1@gmail.com>
2020-04-08 13:53:19 -0500

    Added PostLink component and local styles.

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -2,35 +2,35 @@ import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
 import fetch from 'isomorphic-unfetch';
 
-const Post = props => (
-    <Layout>
-        <h1>{props.show.name}</h1>
-        <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-        {props.show.image ? <img src={props.show.image.medium} /> : null}
-    </Layout>
-)
+// const Post = props => (
+//     <Layout>
+//         <h1>{props.show.name}</h1>
+//         <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+//         {props.show.image ? <img src={props.show.image.medium} /> : null}
+//     </Layout>
+// )
 
-Post.getInitialProps = async function(context) {
-    const { id } = context.query;
-    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
-    const show = await res.json();
+// Post.getInitialProps = async function(context) {
+//     const { id } = context.query;
+//     const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+//     const show = await res.json();
 
-    console.log(`Fetched show: ${show.name}`);
+//     console.log(`Fetched show: ${show.name}`);
 
-    return { show }
-}
+//     return { show }
+// }
 
-export default Post;
+// export default Post;
 
 // ======== LINKS WITH DYNAMIC ROUTING =========
 //
-// export default function Post() {
-//     const router = useRouter();
+export default function Post() {
+    const router = useRouter();
 
-//     return (
-//         <Layout>
-//             <h1>{ router.query.id }</h1>
-//             <p>This is the blog post content.</p>
-//         </Layout>
-//     );
-// }
+    return (
+        <Layout>
+            <h1>{ router.query.id }</h1>
+            <p>This is the blog post content.</p>
+        </Layout>
+    );
+}

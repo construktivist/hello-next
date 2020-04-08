e786fc012b741bb3546696f639bc60bb06195bc2
Keegan Kelly <keegankelly1@gmail.com>
2020-04-08 14:09:40 -0500

    Added global styles.

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,7 +1,52 @@
 import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
+import Markdown from 'react-markdown';
 import fetch from 'isomorphic-unfetch';
 
+export default () => {
+    const router = useRouter();
+    return (
+        <Layout>
+            <h1>{router.query.id}</h1>
+            <div className="markdown">
+        <Markdown
+          source={`
+            This is our blog post.
+            Yes. We can have a [link](/link).
+            And we can have a title as well.
+
+            ### This is a title
+
+            And here's the content.
+        `}
+        />
+      </div>
+      <style jsx global>{`
+        .markdown {
+          font-family: 'Arial';
+        }
+
+        .markdown a {
+          text-decoration: none;
+          color: blue;
+        }
+
+        .markdown a:hover {
+          opacity: 0.6;
+        }
+
+        .markdown h3 {
+          margin: 0;
+          padding: 0;
+          text-transform: uppercase;
+        }
+      `}</style>
+        </Layout>
+    )
+}
+
+//======== BLOG POST FOR DISPLAYING FETCHED DATA =========
+//
 // const Post = props => (
 //     <Layout>
 //         <h1>{props.show.name}</h1>
@@ -24,13 +69,13 @@ import fetch from 'isomorphic-unfetch';
 
 // ======== LINKS WITH DYNAMIC ROUTING =========
 //
-export default function Post() {
-    const router = useRouter();
+// export default function Post() {
+//     const router = useRouter();
 
-    return (
-        <Layout>
-            <h1>{ router.query.id }</h1>
-            <p>This is the blog post content.</p>
-        </Layout>
-    );
-}
+//     return (
+//         <Layout>
+//             <h1>{ router.query.id }</h1>
+//             <p>This is the blog post content.</p>
+//         </Layout>
+//     );
+// }

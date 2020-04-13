c2211bd7232aca8041240a219a2d6762f2050184
Keegan Kelly <keegankelly1@gmail.com>
2020-04-13 11:52:44 -0500

    API route using the next.js router.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 //Dependencies
 import Link from 'next/link';
 import fetch from 'isomorphic-unfetch';
+import { useRouter } from 'next/router';
 import useSWR from 'swr';
 
 //Components
@@ -8,118 +9,122 @@ import Layout from '../components/MyLayout';
 
 // ======== API ROUTES =========
 
-// function fetcher(url) {
-//   return fetch(url).then(r.json());
-// }
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
 
-// export default function Index() {
-//   const {data, error} = useSWR('/api/randomQuote', fetcher);
-//   // The following line has optional chaining, added in Next.js v9.1.5,
-//   // is the same as `data && data.author`
-//   const author = data?.author;
-//   let quote = data?.quote;
+export default function Index() {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
 
-//   if (!data) quote = 'Loading...';
-//   if (error) quote = 'Failed to fetch the quote.';
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
 
-//   return (
-//     <main className="center">
-//       <div className="quote">{quote}</div>
-//       {author && <span className="author">- {author}</span>}
+  return (
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
 
-//       <style jsx>{`
-//         main {
-//           width: 90%;
-//           max-width: 900px;
-//           margin: 300px auto;
-//           text-align: center;
-//         }
-//         .quote {
-//           font-family: cursive;
-//           color: #e243de;
-//           font-size: 24px;
-//           padding-bottom: 10px;
-//         }
-//         .author {
-//           font-family: sans-serif;
-//           color: #559834;
-//           font-size: 20px;
-//         }
-//       `}</style>
-//     </main>
-//   );
-// }
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
+        }
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
+        }
+      `}</style>
+    </main>
+  );
+}
 
 // ======== STYLING COMPONENTS =========
 
-function getPosts() {
-    return [
-        {id: 'hello-next.js', title: 'Hello Next.js'},
-        {id: 'learn-next.js', title: 'Learn Next.js is awesome'},
-        {id: 'hello-next.js', title: 'Deploy apps with ZEIT'}
-    ]
-}
+// function getPosts() {
+//     return [
+//         {id: 'hello-next.js', title: 'Hello Next.js'},
+//         {id: 'learn-next.js', title: 'Learn Next.js is awesome'},
+//         {id: 'hello-next.js', title: 'Deploy apps with ZEIT'}
+//     ]
+// }
 
-const PostLink = ({ post }) => (
-  <li>
-    <Link href="/p/[id]" as={'/p/${post.id}'}>
-      <a>{post.title}</a>
-    </Link>
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
+// const PostLink = ({ post }) => (
+//   <li>
+//     <Link href="/p/[id]" as={'/p/${post.id}'}>
+//       <a>{post.title}</a>
+//     </Link>
+//     <style jsx>{`
+//       li {
+//         list-style: none;
+//         margin: 5px 0;
+//       }
 
-      a {
-        text-decoration: none;
-        color: blue;
-        font-family: 'Arial';
-      }
+//       a {
+//         text-decoration: none;
+//         color: blue;
+//         font-family: 'Arial';
+//       }
 
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
-)
+//       a:hover {
+//         opacity: 0.6;
+//       }
+//     `}</style>
+//   </li>
+// )
 
-export default function Blog() {
-    return (
-        <Layout>
-        <h1>My Blog</h1>
-        <ul>
-          {getPosts().map(post => (
-                <PostLink key={post.id} post={post} />
-          ))}
-        </ul>
-        <style jsx>{`
-          h1,
-          a {
-            font-family: 'Arial';
-          }
+// export default function Blog() {
+//     return (
+//         <Layout>
+//         <h1>My Blog</h1>
+//         <ul>
+//           {getPosts().map(post => (
+//                 <PostLink key={post.id} post={post} />
+//           ))}
+//         </ul>
+//         <style jsx>{`
+//           h1,
+//           a {
+//             font-family: 'Arial';
+//           }
   
-          ul {
-            padding: 0;
-          }
+//           ul {
+//             padding: 0;
+//           }
   
-          li {
-            list-style: none;
-            margin: 5px 0;
-          }
+//           li {
+//             list-style: none;
+//             margin: 5px 0;
+//           }
   
-          a {
-            text-decoration: none;
-            color: blue;
-          }
+//           a {
+//             text-decoration: none;
+//             color: blue;
+//           }
   
-          a:hover {
-            opacity: 0.6;
-          }
-        `}</style>
-      </Layout>
-    )
-}
+//           a:hover {
+//             opacity: 0.6;
+//           }
+//         `}</style>
+//       </Layout>
+//     )
+// }
 // ======== FETCHING DATA FOR PAGES =========
 
 // const Index = props => (
